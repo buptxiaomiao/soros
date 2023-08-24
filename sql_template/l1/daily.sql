898c6f9aff68aa2f866e322940d894d3acf56185
wjh-desktop <buptwjh@outlook.com>
2023-08-25 00:34:03 +0800

    finish l1.daily

--- sql_template/l1/daily.sql
+++ sql_template/l1/daily.sql
@@ -4,10 +4,12 @@ create table if not exists l1.daily (
     ts_code             string      comment 'TS代码',
     name                string      comment '股票名称',
     industry            string      comment '股票行业',
+    market              string      comment '市场类型（主板/创业板/科创板/CDR）',
+    is_hs               string      comment '是否是沪深股通: H/S',
     trade_date          string      comment '交易日期',
 
     total_mv            float       comment '总市值（亿元）',
-    total_mv_wan        float       comment '总市值（万元）'
+    circ_mv             float       comment '流通市值（亿元）',
     amount              float       comment '成交额（万元）',
     turnover_rate       float       comment '换手率（%）',
     turnover_rate_f     float       comment '换手率（自由流通股）',
@@ -35,16 +37,33 @@ stored as orc;
 insert overwrite table l1.daily
 select
     t1.ts_code,
-    t3.name,
-    t3.industry,
+    dim_stock.name,
+    dim_stock.industry,
+    dim_stock.market,
+    dim_stock.is_hs,
     t1.trade_date,
     t2.total_mv,
     t2.total_mv_wan,
-    t2.amount,
+    t1.amount,
     t2.turnover_rate,
     t2.turnober_rate_f,
     t1.change_pct,
 
+    round(t1.open * adj.factor, 2) as open_qfq,
+    round(t1.high * adj.factor, 2) as high_qfq,
+    round(t1.low * adj.factor, 2) as low_qfq,
+    round(t1.close * adj.factor, 2) as close_qfq,
+    round(t1.preclose * adj.factor, 2) as preclose_qfq,
+    round(t1.vol * adj.factor, 2) as vol_qfq,
+    round(t1.change * adj.factor, 2) as change_qfq,
+
+    t1.open as open_real,
+    t1.high as high_real,
+    t1.low as low_real,
+    t1.close as close_real,
+    t1.preclose as preclose_real,
+    t1.vol as vol_real,
+    t1.change as change_real
 
 from (
     select *
@@ -58,18 +77,32 @@ left join (
     where pt_dt = '9999-01-01'
 ) t2
     on t1.ts_code = t2.ts_code
-    and t1.trade_date = t3.trade_date
+    and t1.trade_date = t2.trade_date
 
 left join l1.dim_stock dim_stock
     on t1.ts_code = dim_stock.ts_code
 
 left join (
     select
-        ts_code,
-        trade_date,
-        adj_factor
-    from ods.adj_factor
-    where pt_dt = '9999-01-01'
+        a.ts_code,
+        a.trade_date,
+        if(a.adj_factor = 1, 1, a.adj_factor / b.last_factor) factor
+    from (
+        select ts_code,
+                trade_date,
+                adj_factor
+        from ods.adj_factor
+        where pt_dt = '9999-01-01'
+     ) a left join (
+         select
+            ts_code,
+            max(adj_factor) last_factor
+         from ods.adj_factor
+         where pt_dt = '9999-01-01'
+         group by
+            ts_code
+    ) b
+    on a.ts_code = b.ts_code
 ) adj
     on t1.ts_code = adj.ts_code
     and t1.trade_date = adj.trade_date
