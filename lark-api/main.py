df8bf52b2d229a9fa1c144b4f178e8ac11fe2608
wjh-desktop <buptwjh@outlook.com>
2023-12-09 00:18:42 +0800

    add lark control

--- lark-api/main.py
+++ lark-api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+import os
 import lark_oapi as lark
 from lark_oapi.adapter.flask import *
 from lark_oapi.api.application.v6 import P2ApplicationBotMenuV6
@@ -8,19 +9,47 @@ from lark_oapi.api.im.v1 import *
 app = Flask(__name__)
 
 
+class Action(object):
+
+    SHUTDOWN_NOW = "shutdown now"
+
+    START_TASK = "cd /root/github/soros && git pull origin master && " \
+                 "cd bash && bash ./ods.sh && cd ../ts && python l1.py"
+
+
+def create_reply(rep):
+    reply = CreateMessageRequest.builder() \
+        .receive_id_type("open_id") \
+        .request_body(CreateMessageRequestBody.builder()
+                      .receive_id('ou_0883cfb95cc4200eb4ee6a5ed06ab47a')
+                      .msg_type('text')
+                      .content(rep)
+                      .build()) \
+        .build()
+    client.im.v1.message.create(reply)
+
+
 def do_p2_im_message_receive_v1(data: P2ImMessageReceiveV1) -> None:
     print(lark.JSON.marshal(data))
-    if data.event:
-        user_id = data.event.sender.sender_id.user_id
-        content = data.event.message.content
-        if user_id == '812a23gf' and '关机' in content:
-            print("receive 关机. from 812a23gf")
-            import os
-            os.system("whoami && uptime")
 
 
 def do_bot_menu(data: P2ApplicationBotMenuV6) -> None:
     print(lark.JSON.marshal(data))
+    open_id = data.event.operator.operator_id.open_id
+    event_key = data.event.event_key
+    if open_id != 'ou_0883cfb95cc4200eb4ee6a5ed06ab47a':
+        return
+
+    if event_key == '关机':
+        create_reply("receive 关机. from ou_088")
+        os.system(Action.SHUTDOWN_NOW)
+
+    elif event_key == '执行任务':
+        create_reply("receive 执行任务. from ou_088")
+        os.system(Action.START_TASK)
+
+    else:
+        create_reply("I am healthy.")
 
 
 def do_customized_event(data: lark.CustomizedEvent) -> None:
@@ -32,6 +61,8 @@ handler = lark.EventDispatcherHandler.builder(lark.ENCRYPT_KEY, lark.VERIFICATIO
     .register_p1_customized_event("message", do_customized_event) \
     .register_p2_application_bot_menu_v6(do_bot_menu) \
     .build()
+
+client = lark.Client.builder().app_id(lark.APP_ID).app_secret(lark.APP_SECRET).build()
 
 
 @app.route("/lark/event", methods=["POST", "GET"])
