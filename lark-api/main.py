cea4f46b3e751763f88954a5532798d21673b7d9
wjh-desktop <buptwjh@outlook.com>
2023-12-08 23:10:17 +0800

    add lark

--- lark-api/main.py
+++ lark-api/main.py
@@ -2,12 +2,24 @@ from flask import Flask
 
 import lark_oapi as lark
 from lark_oapi.adapter.flask import *
+from lark_oapi.api.application.v6 import P2ApplicationBotMenuV6
 from lark_oapi.api.im.v1 import *
 
 app = Flask(__name__)
 
 
 def do_p2_im_message_receive_v1(data: P2ImMessageReceiveV1) -> None:
+    print(lark.JSON.marshal(data))
+    if data.event:
+        user_id = data.event.sender.sender_id.user_id
+        content = data.event.message.content
+        if user_id == '812a23gf' and '关机' in content:
+            print("receive 关机. from 812a23gf")
+            import os
+            os.system("whoami && uptime")
+
+
+def do_bot_menu(data: P2ApplicationBotMenuV6) -> None:
     print(lark.JSON.marshal(data))
 
 
@@ -18,6 +30,7 @@ def do_customized_event(data: lark.CustomizedEvent) -> None:
 handler = lark.EventDispatcherHandler.builder(lark.ENCRYPT_KEY, lark.VERIFICATION_TOKEN, lark.LogLevel.DEBUG) \
     .register_p2_im_message_receive_v1(do_p2_im_message_receive_v1) \
     .register_p1_customized_event("message", do_customized_event) \
+    .register_p2_application_bot_menu_v6(do_bot_menu) \
     .build()
 
 
