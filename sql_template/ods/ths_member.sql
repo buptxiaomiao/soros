b8dbc1ec89be4afe7a5b4c816d3698890b52004c
wjh-desktop <buptwjh@outlook.com>
2024-01-03 01:18:08 +0800

    ods 增加同花顺概念 成分

---
+++ sql_template/ods/ths_member.sql
@@ -0,0 +1,18 @@
+
+#set table_name = 'ths_member'
+#set unique_cols = 'ts_code,code'
+#set cols = 'ts_code,code,`name`,weight,in_date,out_date,is_new'
+
+-- ddl
+create table if not exists ods_incr.${table_name} (
+    ts_code         string      comment 'TS代码',
+    code            string      comment '股票代码',
+    name            string      comment '股票名称',
+    `weight`        float       comment '权重',
+    in_date         string      comment '纳入日期',
+    out_date        string      comment '剔除日期',
+    `is_new`        string      comment '是否最新Y是N否'
+)  comment '同花顺概念成分'
+partitioned by (
+    pt_dt   string  comment '分区时间yyyy-mm-dd'
+) row format delimited fields terminated by '\u0001' stored as textfile;
---
+++ ts/ths_member.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+import time
+
+import pandas as pd
+
+from base_task import BaseTask
+
+
+class ThsMember(BaseTask):
+
+    DATA_FILE = 'ths_member.csv'
+    SQL_FILE = 'ths_member.sql'
+
+    SLEEP_SECONDS = 0.5
+
+    @classmethod
+    def run(cls):
+        return cls.run_by_dt()
+
+    @classmethod
+    def get_df(cls, *args, **kwargs):
+        limit = kwargs.get('limit', 5000)
+        offset = kwargs.get('offset', 0)
+        df = cls.pro.ths_member(**{
+            "ts_code": "",
+            "code": "",
+            "limit": 5000,
+            "offset": 0
+        }, fields=[
+            "ts_code",
+            "code",
+            "name",
+            "weight",
+            "in_date",
+            "out_date",
+            "is_new"
+        ])
+
+        if df.count < 5000:
+            return df
+
+        if cls.SLEEP_SECONDS:
+            time.sleep(cls.SLEEP_SECONDS)
+        new_offset = limit + offset
+        return pd.concat([df, cls.get_df(limit=limit, offset=new_offset)])
+
+
+if __name__ == '__main__':
+    ThsMember.run()
